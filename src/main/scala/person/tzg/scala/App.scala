970dc1bcf6969c60854038608a17477d59228d2a
admin <admin>
2016-11-12 18:18:31 +0800

    init develop context

--- src/main/scala/person/tzg/scala/App.scala
+++ src/main/scala/person/tzg/scala/App.scala
@@ -1,11 +1,13 @@
 package person.tzg.scala
 
+import org.apache.spark.{SparkContext, SparkConf}
+
 /**
  * Created by hadoop on 11/11/16.
  */
 object Exe1 extends Serializable{
   def main(args:Array[String]): Unit= {
-    val conf = new SparkConf().setMaster("local[2]").setAppName("Exe1").set("spark.executor.memory")
+    val conf = new SparkConf().setMaster("local[2]").setAppName("Exe1")
     val sc = new SparkContext(conf)
     val kv1 = sc.parallelize(List(("A", 1), ("B", 2), ("C", 3), ("A", 4), ("B", 5)))
     kv1.sortByKey().collect
