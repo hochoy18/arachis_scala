085ecfb498c17a7bf796c1efe0bd1f5d5aa3fa76
tongzhenguo <tzg@meitu.com>
2018-06-22 17:28:11 +0800

    1.新增scala 字符串插值用例

--- README.md
+++ README.md
@@ -2,14 +2,11 @@
 ---------
 #### 模块说明
 - SClearn scala语法学习用例
+    - 字符串插值:Var$UserCase
+    - 隐式转换:ImplicitTransferLearn
+    - 模式匹配:MatchUserCase
 - model 机器学习模型实现
 - org.apache.spark  spark相关
 - person.tzg.scala 自己实现的scala/spark程序
 - scopt 用来解析参数的scala包,相当于python中的ConfigParser
 - utils 各工具包
-
-
-
-
-
-
---
+++ src/main/scala/SClearn/Var$UserCase.scala
@@ -0,0 +1,27 @@
+package SClearn
+
+/**
+  *
+  * Created by tongzhenguo on 2018/6/22.
+  * 使用$插入变量值,优化代码可读性
+  * [字符串插值](https://docs.scala-lang.org/zh-cn/overviews/core/string-interpolation.html)
+  */
+object Var$UserCase {
+
+  def main(args: Array[String]): Unit = {
+
+    val signl = 10086
+    // 原来的写法
+    println("signl = %s".format(signl))
+    println("signl = val".replaceAll("val",signl.toString))
+
+    // 优化写法
+    println(s"signl = $signl")
+    // 表达式写法
+    println(s"signl = ${signl.toDouble}")
+    // 格式化写法
+    println(f"signl = ${signl.toDouble}%2.2f")
+  }
+
+
+}
